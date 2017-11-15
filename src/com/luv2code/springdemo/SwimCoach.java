f12ce227d035cd4b9eb42198f3156782197d8833
Jaikant Singh <jaikantjk@gmail.com>
2017-11-15 09:00:30 +0530

    First commit

---
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+public class SwimCoach implements Coach {
+
+    private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+	this.fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Swim 1000 ,eters as a warm up.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
+	}
+
+}
